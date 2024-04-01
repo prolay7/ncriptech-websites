e8b36241c4001dfb2accab6b6b3ceaf51c30a603
prolayncriptech <163509438+prolayncriptech@users.noreply.github.com>
2024-04-01 18:27:14 +0530

    add header and footer section

---
+++ rangamati_institute_of_nursing/index.php
@@ -0,0 +1,40 @@
+<?php 
+include_once('config/config.php'); 
+?>
+<!doctype html>
+<html lang="zxx">
+
+<!-- Mirrored from templates.hibootstrap.com/eduon/default/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 26 Feb 2024 07:11:54 GMT -->
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel='stylesheet' href='public/plugins/goodlayers-core/plugins/combine/style.css' type='text/css' media='all' />
+    <link rel='stylesheet' href='public/plugins/goodlayers-core/include/css/page-builder.css' type='text/css' media='all' />
+    <link rel='stylesheet' href='public/plugins/revslider/public/assets/css/settings.css' type='text/css' media='all' />
+    <link rel='stylesheet' href='public/css/style-core.css' type='text/css' media='all' />
+    <link rel='stylesheet' href='public/css/kingster-style-custom.css' type='text/css' media='all' />
+
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700%2C400" rel="stylesheet" property="stylesheet" type="text/css" media="all">
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Poppins%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2Cregular%2Citalic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CABeeZee%3Aregular%2Citalic&amp;subset=latin%2Clatin-ext%2Cdevanagari&amp;ver=5.0.3' type='text/css' media='all' />
+
+<link rel="apple-touch-icon" sizes="76x76" href="public/img/icon/favicon/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="152x152" href="public/img/icon/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="public/img/icon/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="public/img/icon/favicon/favicon-16x16.png">
+<link rel="manifest" href="public/img/icon/favicon/site.webmanifest">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+
+
+<title>Rangamati institute of nursing</title>
+</head>
+<body>
+
+
+<?php
+include_once('includes/common/header_menu.php'); 
+include_once('includes/pages/pages.php'); 
+include_once('includes/common/footer.php'); 
+?>
